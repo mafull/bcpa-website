ac695a9d50fe47fc01e9c89d68e1fcc51f9ade7f
Max Fuller <max.fuller@cmrsurgical.com>
2019-05-05 14:02:03 +0100

    Missing file from previous commit

---
+++ frontend/src/state/events/index.tsx
@@ -0,0 +1,13 @@
+
+
+const INITIAL_STATE = {
+    items: ["Event 1", "Event 2", "Event 3", "Event 4"]
+};
+
+const reducer = (state = INITIAL_STATE, action: any) => {
+    switch (action.type) {
+    default: return state;
+    }
+};
+
+export default reducer;
--- frontend/src/ui/App.tsx
+++ frontend/src/ui/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Container    from "react-bootstrap/Container";
+
 import Footer from "ui/components/Footer";
 import Header from "ui/components/Header";
 
@@ -7,7 +9,9 @@ import Header from "ui/components/Header";
 const App: React.FC<{}> = ({ children: pages }) => (
     <div>
         <Header />
-        {pages}
+        <Container id="main-content">
+            {pages}
+        </Container>
         <Footer />
     </div>
 );

c6a14e8cce89cc9e003ca487186e2c0e36ceb943
Max Fuller <max.fuller@cmrsurgical.com>
2019-05-05 19:24:46 +0100

    Improve header

--- frontend/src/ui/components/Header.tsx
+++ frontend/src/ui/components/Header.tsx
@@ -1,47 +1,44 @@
 import React    from "react";
 import { Link } from "react-router-dom";
 
-import Nav      from "react-bootstrap/Nav";
-import Navbar   from "react-bootstrap/Navbar";
-
-
-// const itemsProperties = [
-//     {
-//         name: "home",
-//         content: "Home",
-//         target: "/"
-//     },
-//     {
-//         name: "events",
-//         content: "Events",
-//         target: "/events"
-//     },
-//     {
-//         name: "login",
-//         content: "Login",
-//         target: "/login"
-//     }
-// ];
+import Container    from "react-bootstrap/Container";
+import Nav          from "react-bootstrap/Nav";
+import Navbar       from "react-bootstrap/Navbar";
 
 
 const Header: React.FC<{}> = () => (
     <div>
-        <Navbar bg="primary" expand="sm" variant="dark">
-            <Navbar.Brand as={Link} to="/" >BCPA</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-                <Nav>
-                    {
-                        ["Events", "Login"].map(name => (
-                            <Nav.Link key={name}
-                                as={Link}
-                                to={name.toLowerCase()}>
-                                {name}
-                            </Nav.Link>
-                        ))
-                    }
+        <Navbar
+            bg="primary"
+            expand="sm"
+            fixed="top"
+            variant="dark"
+        >
+            <Container>
+                <Navbar.Brand as={Link} to="/" >BCPA</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-center">
+                    <Nav>
+                        {
+                            ["Events", "Clubs", "Leagues"].map(name => (
+                                <Nav.Link key={name}
+                                    as={Link}
+                                    to={name.toLowerCase()}>
+                                    {name}
+                                </Nav.Link>
+                            ))
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+                <Nav className="justify-content-end">
+                    <Nav.Link
+                        as={Link}
+
+                        to="login">
+                        Login
+                    </Nav.Link>
                 </Nav>
-            </Navbar.Collapse>
+            </Container>
         </Navbar>
     </div>
 );
